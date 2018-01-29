6acbcdd047ee64f90a3c73828cd8f81cb905bac6
KuoYu-chiung <genekuo@cybersoft4u.com>
2018-01-29 14:02:56 +0800

    Adding node server troppshere script

--- nodeserver-cf-template.py
+++ nodeserver-cf-template.py
@@ -101,23 +101,9 @@ t.add_resource(InstanceProfile(
     Roles=[Ref("Role")]
 ))
 
-t.add_resource(IAMPolicy(
-	"Policy",
-	PolicyName="AllowS3",
-	PolicyDocument=Policy(
-		Statement=[
-		Statement(
-			Effect=Allow,
-			Action=[Action("s3", "*")],
-			Resource=["*"])
-		]
-	),
-	Roles=[Ref("Role")]
-))
-
 t.add_resource(ec2.Instance(
     "instance",
-    ImageId="ami-caaf84af",
+    ImageId="ami-a4c7edb2",
     InstanceType="t2.micro",
     SecurityGroups=[Ref("SecurityGroup")],
     KeyName=Ref("KeyPair"),
